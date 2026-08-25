47e800af841ab331a927bf6630d3c371a59387ef
purej1983 <purej1983@gmail.com>
2026-08-26 05:14:41 +0800

    feat: use distinctUntilChanged to dropping consecutive duplicates signal

--- src/main/kotlin/practice/week3/FlowOperatorsPractice.kt
+++ src/main/kotlin/practice/week3/FlowOperatorsPractice.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlin.coroutines.cancellation.CancellationException
 import kotlin.time.Duration.Companion.milliseconds
 
@@ -114,7 +115,7 @@ object FlowOperatorsPractice {
      * Requirement: use `distinctUntilChanged`.
      */
     fun distinctQueries(queries: Flow<String>): Flow<String> {
-        TODO()
+        return queries.distinctUntilChanged()
     }
 
     /**
